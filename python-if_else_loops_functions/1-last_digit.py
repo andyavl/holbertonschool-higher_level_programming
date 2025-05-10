191be05ec0cd7d68001be5ac0a15a94b5560a97f
andyavl <andyavl@hotmail.com>
2025-05-10 19:14:50 -0400

    1. The last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    lastdigit = -number % 10 * -1
+else:
+    lastdigit = number % 10
+
+if lastdigit > 5:
+    phrase = "greater than 5"
+elif lastdigit == 0:
+    phrase = "0"
+else:
+    phrase = "less then 6 and not 0"
+
+print(f"Last digit of {number} is {lastdigit} and is {phrase}")
--- python-if_else_loops_functions/README.md
+++ python-if_else_loops_functions/README.md
@@ -3,3 +3,4 @@
 ## Tasks:
 
 0. Positive anything is better than negative nothing
+1. The last digit
