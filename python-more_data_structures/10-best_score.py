7f303c96ca3b0883110482923f4514e926d3d65f
andyavl <andyavl@hotmail.com>
2025-05-16 12:48:36 -0400

    10. Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,6 +4,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
+    who = next(iter(a_dictionary))
     best = next(iter(a_dictionary.values()))
     for key in a_dictionary:
         if a_dictionary[key] > best:
