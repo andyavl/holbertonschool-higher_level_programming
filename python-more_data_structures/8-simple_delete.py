8bc53cf6ca9bfbb1e4b3280503c8293c086ffe43
andyavl <andyavl@hotmail.com>
2025-05-16 11:26:01 -0400

    8. Simple delete by key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -3,6 +3,6 @@
 def simple_delete(a_dictionary, key=""):
     for i in a_dictionary:
         if i == key:
-            del(a_dictionary[key])
+            del (a_dictionary[key])
             return a_dictionary
     return a_dictionary
