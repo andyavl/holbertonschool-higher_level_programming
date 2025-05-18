d314fd008001b96dbe8ef4cc86ce58122a3de22a
andyavl <andyavl@hotmail.com>
2025-05-17 21:52:28 -0400

    12. Roman to Integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    if not roman_string:
+        return 0
+
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    numbers = [roman_dict[char] for char in roman_string]
+
+    for i in range(1, len(numbers)):
+        if numbers[i] > numbers[i - 1]:
+            numbers[i - 1] = -numbers[i - 1]
+
+    return sum(numbers)
