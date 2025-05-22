ed856cff523a464ace752e6e3845286ad8e6f675
andyavl <andyavl@hotmail.com>
2025-05-21 21:38:45 -0400

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Defines an empty class Square."""
+
 class Square:
+    """Represents a square.
+    
+    This class is currently empty and serves as a placeholder
+    for future implementation of square-related functionality.
+    """
     pass
