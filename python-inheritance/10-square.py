057b5dd5696ea5b1b960233a67d5cbff3c456256
andyavl <andyavl@hotmail.com>
2025-05-29 21:55:03 -0400

    10. Square #1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -33,38 +33,7 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
 
 
-class Rectangle(BaseGeometry):
-    """
-    Represents a rectangle using validated width and height.
-    Inherits from BaseGeometry.
-    """
-
-    def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance after validating the dimensions.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Calculates the area of the rectangle.
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Returns the string representation of the rectangle.
-        """
-        return "[{}] {}/{}".format(self.__class__.__name__,
-                                   self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
