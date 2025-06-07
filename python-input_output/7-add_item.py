f6b4bf3a1207832ec92cf45b9542bc8b1b9cc8ff
andyavl <andyavl@hotmail.com>
2025-06-07 14:50:10 -0400

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,19 +4,28 @@ Script that saves command-line arguments to a JSON file.
 
 This script:
 - Accepts command-line arguments (excluding the script name itself).
-- Saves them into a JSON file named 'add_item.json'.
-- Then loads the contents of the file back into a Python object.
+- Loads existing data from 'add_item.json' if it exists.
+- Appends new arguments to the list.
+- Saves the updated list to 'add_item.json'.
 """
 
 import sys
+import os
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
+filename = "add_item.json"
 arg_list = sys.argv[1:]
 
-filename = "add_item.json"
+if os.path.exists(filename):
+    try:
+        data = load_from_json_file(filename)
+    except Exception:
+        data = []
+else:
+    data = []
 
-save_to_json_file(arg_list, filename)
+data.extend(arg_list)
 
-load_from_json_file(filename)
+save_to_json_file(data, filename)
